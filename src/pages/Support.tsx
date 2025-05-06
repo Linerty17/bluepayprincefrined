dee4e534b5104ca2d2d11d010ea222683daabef3
Linerty17 <sundaychinemerem66@gmail.com>
2025-05-06 21:59:36 +0100

    Update Support.tsx

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -51,7 +51,7 @@ const Support = () => {
             </div>
             <Button 
               className="w-full mt-4 bg-blue-600 hover:bg-blue-700"
-              onClick={() => window.open('https://t.me/Officialbluepay')}
+              onClick={() => window.open('https://t.me/bluepay_service')}
             >
               Open Telegram
             </Button>
