67e25625698dd08138625ba2452ced6179c20db3
Linerty17 <sundaychinemerem66@gmail.com>
2025-05-06 22:06:59 +0100

    Update AirtimePurchase.tsx

--- src/pages/AirtimePurchase.tsx
+++ src/pages/AirtimePurchase.tsx
@@ -45,7 +45,7 @@ const AirtimePurchase = () => {
     }
 
     // Validate BPC code
-    if (bpcCode !== "BPC816222") {
+    if (bpcCode !== "BPC663737") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
